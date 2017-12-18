9d8b3e90d4899b245132c8a625c5d3941983eddb
jeanecard <jean.ecard@outlook.fr>
2017-12-18 11:23:01 +0100

    Add files via upload

---
+++ HRCountriesServicesSolution/App_Start/FilterConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace HRCountriesServicesSolution
+{
+    /// <summary>
+    /// 
+    /// </summary>
+
+    public class FilterConfig
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filters"></param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HRCountriesServicesSolution/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using HRCountriesServicesSolution.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace HRCountriesServicesSolution
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class WebApiConfig
+    {
+        /// <summary>
+        /// Lancement des factories et autres services, configurations nécessaires au Web service
+        /// </summary>
+        /// <param name="config"></param>
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+            //Démarrage de l'injecteur de dépendance.
+            StructuremapWebApi.Start();
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
